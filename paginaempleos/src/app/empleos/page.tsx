070a5127268767512e727c7b3af16448b66d8110
TOConde <tomy_t.c@hotmail.com>
2023-10-27 19:39:14 -0300

    creo las otras 2 paginas y le pongo navBar

---
+++ paginaempleos/src/app/empleos/page.tsx
@@ -0,0 +1,11 @@
+import { NavBar } from "../componentes/navBar/navBar";
+
+export default function EmpleosPage () {
+  return (
+    <>
+      <h1>Empleos page</h1>
+      <NavBar />
+    </>
+    
+  )
+}
