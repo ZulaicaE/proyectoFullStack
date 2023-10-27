b3e624c37db57730ecd85eaa65277d9ee450ee42
TOConde <tomy_t.c@hotmail.com>
2023-10-27 19:36:38 -0300

    instale bootstrap, no se si queda para todos :)

--- paginaempleos/src/app/page.tsx
+++ paginaempleos/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import styles from './page.module.css';
 import { Banner } from "./componentes/banner/banner";
 import { NavBar } from "./componentes/navBar/navBar";
