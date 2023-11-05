b325afef4fd234663df1c4641ff0ce8c2503bdd3
ZulaicaE <zulaicaemiliano@gmail.com>
2023-11-05 19:46:37 -0300

    Font y Menu

--- paginaempleos/src/app/componentes/homeMenu/HomeMenu.tsx
+++ paginaempleos/src/app/componentes/homeMenu/HomeMenu.tsx
@@ -4,38 +4,32 @@ import Link from "next/link"
 const links = [{
     label: 'Noticias',
     route: '/noticias',
-    // image: './img/navBar/home.svg'
 }, {
     label: 'Salud',
     route: '/empleos',
-    // image: './img/navBar/factory.png'
 }, {
     label: 'Deportes',
     route: '/deportes',
-    // image: './img/navBar/worker.png'
 },
 {
     label: 'Servicios',
     route: '/servicios',
-    // image: './img/navBar/worker.png'
 },
 {
     label: 'Transporte',
     route: '/transporte',
-    // image: './img/navBar/worker.png'
 },
 {
     label: 'Turismo',
     route: '/turismo',
-    // image: './img/navBar/worker.png'
 }]
 
 export const HomeMenu = () => {
     return (
         <div className={styles.menu}>
-            <ul className="">
+            <ul className={`${styles.navMenu} list-group list-group-flush`}>
                 {links.map(({ label, route }) => (
-                    <li key={route}>
+                    <li className={`${styles.navMenu} list-group-item`} key={route}>
                         <Link href={route}>
                             {label}
                         </Link>
