b5d5a69dcefe97edb460ab1b17f7c9a62eca008b
ZulaicaE <zulaicaemiliano@gmail.com>
2023-11-05 18:37:28 -0300

    Banner Responsive

--- paginaempleos/src/app/componentes/bannerHome/Banner.tsx
+++ paginaempleos/src/app/componentes/bannerHome/Banner.tsx
@@ -2,8 +2,8 @@ import './Banner.css';
 
 export const Banner = () => {
   return (
-    <div className='banner'>
-      <img src="/img/banner/cityHall3.jpg" alt="Banner" />
+    <div className="divBanner">
+      <img className="imgBanner img-fluid" src="/img/banner/cityHall3.jpg" alt="Banner" />
     </div>
   )
 }
