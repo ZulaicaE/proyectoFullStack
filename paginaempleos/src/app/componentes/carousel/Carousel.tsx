0aa0112d5552517e55eec86c340f0545b81e5c63
TOConde <tomy_t.c@hotmail.com>
2023-11-06 16:58:03 -0300

    Agrego imagenes de carousel

--- paginaempleos/src/app/componentes/carousel/Carousel.tsx
+++ paginaempleos/src/app/componentes/carousel/Carousel.tsx
@@ -4,32 +4,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "../Carousel/Carousel.module.css";
 
 const slides = [{
-    id: 1,
-    desc: 'Lorem ipsum.',
-    image: './img/carousel/slide-placeholder.png'
+  id: 1,
+  desc: 'Lorem ipsum.',
+  image: './img/carousel/city1.jpg'
 }, {
-    id: 2,
-    desc: 'Lorem ipsum.',
-    image: './img/carousel/slide-placeholder.png'
+  id: 2,
+  desc: 'Lorem ipsum.',
+  image: './img/carousel/city2.png'
 }, {
-    id: 3,
-    desc: 'Lorem ipsum.',
-    image: './img/carousel/slide-placeholder.png'
+  id: 3,
+  desc: 'Lorem ipsum.',
+  image: './img/carousel/city4.webp'
 }]
 
 export const ImageCarousel = () => {
   const [index, setIndex] = useState(0);
-  
+
   const handleSelect = (selectedIndex: number) => {
     setIndex(selectedIndex);
   };
   return (
     <Carousel activeIndex={index} onSelect={handleSelect} className={styles.imageCarousel} pause={"hover"} indicators={false}>
-        {slides.map(({id, desc, image}) => (
-          <Carousel.Item key={id} className={styles.itemCarousel} interval={4000}>
-            <img src={image} alt={desc}/>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-      );
+      {slides.map(({ id, desc, image }) => (
+        <Carousel.Item key={id} className={styles.itemCarousel} interval={4000}>
+          <img src={image} alt={desc} />
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
 }
