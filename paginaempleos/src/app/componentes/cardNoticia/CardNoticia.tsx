b259b261b3f0fb2ceac2f5b8b72175fb31a40522
ZulaicaE <zulaicaemiliano@gmail.com>
2023-11-06 20:13:55 -0300

    Cambios en noticias

--- paginaempleos/src/app/componentes/cardNoticia/CardNoticia.tsx
+++ paginaempleos/src/app/componentes/cardNoticia/CardNoticia.tsx
@@ -1,4 +1,4 @@
-import styles from './cardNoticia.module.css';
+import styles from './CardNoticia.module.css';
 
 const news = [{
     noticia: 'Accidente',
@@ -6,30 +6,31 @@ const news = [{
     image: './img/news/crashShip.jpg'
 }, {
     noticia: 'Exploracion',
-    info: 'Enterate quienes son los astronautas que entraran por primera vez al agujero negro Sagitario A.',
+    info: 'Enterate quienes son los astronautas que entraran por primera vez al gigante agujero negro Sagitario A.',
     image: './img/news/astronauts.jpg'
 }, {
     noticia: 'Incendio',
-    info: 'No solo los humanos sientes el llamado de heroe, los robots no dudan a enfrentar las llamas.',
+    info: 'No solo los humanos sienten el llamado de heroe, los robots no dudan a enfrentar las llamas.',
     image: './img/news/robotSavesRobot.jpg'
 }]
 
 
 export const CardNoticia = () => {
     return (
-        <div className={styles.noticia}>
+        <div className={styles.cardNoticia}>
             <div className='row'>
                 {news.map(({ noticia, info, image }) => (
                     <div className='col' key={noticia}>
-                        <div className="card" style={{ width: '18rem' }}>
+                        <div className={styles.card}>
                             <img src={image} className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <p className="card-text">{info}</p>
-                            </div>
+                                <div className={styles.cardBody}>
+                                    <h5 className="card-title">{noticia}</h5>
+                                    <p className="card-text">{info}</p>
+                                </div>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
-    )
+        )
 }
