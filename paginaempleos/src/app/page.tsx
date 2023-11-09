5d9677204554ad6b5f31577a0d3debb28fcb0492
ZulaicaE <zulaicaemiliano@gmail.com>
2023-11-08 22:11:58 -0300

    padding entre noticias

--- paginaempleos/src/app/page.tsx
+++ paginaempleos/src/app/page.tsx
@@ -17,10 +17,10 @@ export default function Home() {
       <main className={styles.main}>
         <h1>¡Bienvenidos a Futurama, la asombrosa metrópolis del año 3000!</h1>
         <div className="d-flex flex-row">
-          <div className='col-2 d-flex flex-row justify-content-evenly'>
+          <div className='col-2 d-flex flex-row justify-content-between'>
             <HomeMenu />
           </div>
-          <div className='col-10 d-flex flex-row justify-content-evenly'>
+          <div className='col-10 d-flex flex-row justify-content-between'>
             <InfoHome />
           </div>
         </div>
