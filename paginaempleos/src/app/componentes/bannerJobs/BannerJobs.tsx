3758d71a14992746b7dcdb003411143378e81c4c
TOConde <tomy_t.c@hotmail.com>
2023-11-14 16:02:04 -0300

    elimino los otros banner que ya no usamos

--- paginaempleos/src/app/componentes/bannerJobs/BannerJobs.tsx
+++
@@ -1,9 +0,0 @@
-import './BannerJobs.css';
-
-export const BannerJobs = () => {
-    return (
-        <div>
-            <img src="/img/bannerJobs/uGottaDo.jpg" className="imgBannerJobs img-fluid" alt="BannerJobs"></img>
-        </div>
-    )
-}
