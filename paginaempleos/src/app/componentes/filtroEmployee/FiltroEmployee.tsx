afb763e3f482e0c8f06f10b3aff104d1e044915a
TOConde <tomy_t.c@hotmail.com>
2023-11-17 19:33:57 -0300

    Fix filtrado en loop

--- paginaempleos/src/app/componentes/filtroEmployee/FiltroEmployee.tsx
+++ paginaempleos/src/app/componentes/filtroEmployee/FiltroEmployee.tsx
@@ -2,38 +2,38 @@ import React, { useState, useEffect } from 'react';
 import DataCandidatos from '../dataCanditatos/DataCandidatos';
 
 interface FiltroEmployeeProps {
-    cambiosCandidatosFiltrados: (candidatos: any[]) => void;
+  cambiosCandidatosFiltrados: (candidatos: any[]) => void;
 }
 
 export const FiltroEmployee: React.FC<FiltroEmployeeProps> = ({ cambiosCandidatosFiltrados }) => {
-    const [filtro, setFiltro] = useState<string>('');
+  const [filtro, setFiltro] = useState<string>('');
 
-    const filtrarCandidatos = (filtrado: string) => {
-        setFiltro(filtrado);
-    };
+  const filtrarCandidatos = (filtrado: string) => {
+    setFiltro(filtrado);
+  };
 
+  useEffect(() => {
     const candidatosFiltrados = DataCandidatos.filter((candidato: any) => {
-        return (
-            candidato.nombre.toLowerCase().includes(filtro.toLowerCase()) ||
-            candidato.apellido.toLowerCase().includes(filtro.toLowerCase()) ||
-            candidato.edad.toLowerCase().includes(filtro.toLowerCase()) ||
-            candidato.fulltime.toLowerCase().includes(filtro.toLowerCase()) ||
-            candidato.movilidad.toLowerCase().includes(filtro.toLowerCase())
-        );
+      return (
+        candidato.nombre.toLowerCase().includes(filtro.toLowerCase()) ||
+        candidato.apellido.toLowerCase().includes(filtro.toLowerCase()) ||
+        candidato.edad.toLowerCase().includes(filtro.toLowerCase()) ||
+        candidato.fulltime.toLowerCase().includes(filtro.toLowerCase()) ||
+        candidato.movilidad.toLowerCase().includes(filtro.toLowerCase())
+      );
     });
 
-    useEffect(() => {
-        cambiosCandidatosFiltrados(candidatosFiltrados);
-    }, [candidatosFiltrados, cambiosCandidatosFiltrados]);
+    cambiosCandidatosFiltrados(candidatosFiltrados);
+  }, [filtro]);
 
-    return (
-        <div>
-            <input
-                type="text"
-                placeholder="Buscar"
-                value={filtro}
-                onChange={(e) => filtrarCandidatos(e.target.value)}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar"
+        value={filtro}
+        onChange={(e) => filtrarCandidatos(e.target.value)}
+      />
+    </div>
+  );
 };
