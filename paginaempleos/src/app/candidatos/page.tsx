bc866c7e9636487a0f3531f0cb70a73f7888c55e
ZulaicaE <zulaicaemiliano@gmail.com>
2023-11-17 19:15:37 -0300

    Layout y filtrado super basico

--- paginaempleos/src/app/candidatos/page.tsx
+++ paginaempleos/src/app/candidatos/page.tsx
@@ -1,11 +1,20 @@
 'use client'
 import styles from './page.module.css';
+import React, { useState, useEffect } from 'react';
 import { NavBar } from "../componentes/navBar/NavBar";
 import { TituloPrincipal } from "../componentes/tituloJobs/TituloPrincipal";
 import { Banner } from '../componentes/banner/Banner';
 import { Employees } from '../componentes/employees/Employees';
+import { FiltroEmployee } from '../componentes/filtroEmployee/FiltroEmployee';
 
 export default function CandidatosPage() {
+
+  const [candidatosFiltrados, setCandidatosFiltrados] = useState([]);
+
+  const manejoCandidatosFiltrados = (candidatos: any) => {
+      setCandidatosFiltrados(candidatos);
+  };
+  
   const src = "/img/banner/thnkBanner.png"
   const titulo = "¡Futurama te necesita! Busque su candidato aquí"
 
@@ -17,12 +26,14 @@ export default function CandidatosPage() {
         <TituloPrincipal titulo={titulo} />
         <div className="d-flex flex-row">
           <div className='col-2 d-flex flex-row justify-content-between align-items-start'>
+          <FiltroEmployee cambiosCandidatosFiltrados={manejoCandidatosFiltrados} />
           </div>
           <div className='col-10 d-flex flex-row justify-content-between'>
-            <Employees />
+            <Employees candidatosFiltrados={candidatosFiltrados} />
           </div>
         </div>
       </main>
     </>
+
   )
 }      
