73f00c29ffbc1b8cd80416f3d0094689b36101e7
TOConde <tomy_t.c@hotmail.com>
2023-11-17 20:24:29 -0300

    arreglo estilo css

--- paginaempleos/src/app/componentes/cardEmployee/CardEmployee.tsx
+++ paginaempleos/src/app/componentes/cardEmployee/CardEmployee.tsx
@@ -1,28 +1,31 @@
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
+import './CardEmployee.css'
 
 interface CardEmployeeProps {
     candidato: {
-      id: string;
-      nombre: string;
-      apellido: string;
-      edad: string;
-      fulltime: string;
-      movilidad: string;
-      img: string;
+        id: string;
+        nombre: string;
+        apellido: string;
+        edad: string;
+        fulltime: string;
+        movilidad: string;
+        img: string;
     };
-  }
-  
-export const CardEmployee: React.FC<CardEmployeeProps> = ({candidato}) => {
+}
+
+export const CardEmployee: React.FC<CardEmployeeProps> = ({ candidato }) => {
     return (
-        <Card>
+        <Card  className='h-100 d-flex flex-column cardEmployee'>
             <Image src={candidato.img} roundedCircle fluid />
-            <Card.Body>
+            <Card.Body className='d-flex flex-column justify-content-between'>
                 <Card.Title>{candidato.nombre}, {candidato.apellido}</Card.Title>
                 <Card.Text>
-                    Edad: {candidato.edad},
-                    Full Time: {candidato.fulltime},
-                    movilidad: {candidato.movilidad}
+                    Edad: {candidato.edad}<br />
+                    Link a cv o linkedin: <br />
+                    Skills (5 max): <br />
+                    Full Time: {candidato.fulltime}<br />
+                    Movilidad: {candidato.movilidad}
                 </Card.Text>
             </Card.Body>
         </Card>
