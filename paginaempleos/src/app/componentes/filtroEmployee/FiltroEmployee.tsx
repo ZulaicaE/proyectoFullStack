e4683b3ae54e1c15754f9a235b1f788b680edb96
Guada-Chojo <gchojohernandez@gmail.com>
2023-12-06 20:50:31 -0300

    Mejora filtro por nombre en candidatos

--- paginaempleos/src/app/componentes/filtroEmployee/FiltroEmployee.tsx
+++ paginaempleos/src/app/componentes/filtroEmployee/FiltroEmployee.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { ListGroup, Button } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import DataCandidatos from '../dataCanditatos/DataCandidatos';
 import styles from "./FiltroEmployee.module.css";
 
@@ -25,10 +25,10 @@ export const FiltroEmployee: React.FC<FiltroEmployeeProps> = ({ cambiosCandidato
     const candidatosFiltrados = DataCandidatos.filter((candidato: any) => {
       return (
         candidato.nombre.toLowerCase().startsWith(filtroNombre.toLowerCase()) ||
-        candidato.apellido.toLowerCase().startsWith(filtroNombre.toLowerCase())
+        candidato.apellido.toLowerCase().startsWith(filtroNombre.toLowerCase()) ||
+        (candidato.nombre + ' ' + candidato.apellido).toLowerCase().includes(filtroNombre.toLowerCase())
       );
     });
-
     cambiosCandidatosFiltrados(candidatosFiltrados);
   }, [filtroNombre]);
 
@@ -43,7 +43,7 @@ export const FiltroEmployee: React.FC<FiltroEmployeeProps> = ({ cambiosCandidato
           (maxEdad === '' || cardEdad <= maxEdad)
         );
       });
-     cambiosCandidatosFiltrados(candidatosFiltrados); 
+      cambiosCandidatosFiltrados(candidatosFiltrados);
     }
   }, [minEdad, maxEdad]);
 
@@ -94,95 +94,95 @@ export const FiltroEmployee: React.FC<FiltroEmployeeProps> = ({ cambiosCandidato
 
   return (
     <div className={styles.divFiltro}>
-      
-        <div className={styles.name}>
-          <p>Nombre y apellido:</p>
-          <input
-            type="text"
-            placeholder="Buscar..."
-            value={filtroNombre}
-            onChange={(e) => filtrarCandidatos(e.target.value)}
-            className={styles.inputBuscar}
-          />
-        </div>
-        <hr className={styles.hr}/>
-        <div className={styles.age}>
-          <p>Edad:</p>
-          <label className={styles.EdadMinMaxContainer}>
-            <span> Min </span>
-            <input 
-              type="number"
-              placeholder='Min. 18'
-              value={minEdad}
-              onChange={(e) => setMinEdad(parseInt(e.target.value))} 
-              min={18}
-              max={99}
-              className={styles.EdadMinMax}
-            />
-          </label>
-          
-          <label className={styles.EdadMinMaxContainer}>
-            <span> Max </span>
-            <input 
-              type="number" 
-              placeholder='Max. 100'
-              value={maxEdad}
-              onChange={(e) => setMaxEdad(parseInt(e.target.value))} 
-              min={19}
-              max={100}
-              className={styles.EdadMinMax}
-            />
-          </label>
-          
-          <Button onClick={handleReset} size='sm' className={styles.btnReset}>Reset</Button>
-        </div>
-        <hr className={styles.hr}/>
-        <div className={`${styles.cargHor} ${styles.checkBoxContainer}`}>
-          <p>Fulltime:</p>
-          <label>
-            <input
-              type="checkbox"
-              checked={fulltimeYes}
-              onChange={() => handleCheckboxChange('Si', 'Si')}
-              className={`form-check-input ${styles.checkBoxHover}`}
-            />
-            <span className={styles.checkBoxHover}> Si </span>
-          </label>
-          
-          <label>
-            <input
-              type="checkbox"
-              checked={fulltimeNo}
-              onChange={() => handleCheckboxChange('No', 'Si')}
-              className={`form-check-input ${styles.checkBoxHover}`}
-            />
-            <span className={styles.checkBoxHover}> No </span>
-          </label>
-        </div>
-        <hr className={styles.hr}/>
-        <div className={`${styles.mov} ${styles.checkBoxContainer}`}>
-          <p>Movilidad:</p>
-          <label>
-            <input
-              type="checkbox"
-              checked={movilidadYes}
-              onChange={() => handleCheckboxChange('Si', 'No')}
-              className={`form-check-input ${styles.checkBoxHover}`}
-            />
-            <span className={styles.checkBoxHover}> Si </span>
-          </label>
-          
-          <label>
-            <input
-              type="checkbox"
-              checked={movilidadNo}
-              onChange={() => handleCheckboxChange('No', 'No')}
-              className={`form-check-input ${styles.checkBoxHover}`}
-            />
-            <span className={styles.checkBoxHover}> No </span>
-          </label>
-        </div>
+
+      <div className={styles.name}>
+        <p>Nombre y apellido:</p>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          value={filtroNombre}
+          onChange={(e) => filtrarCandidatos(e.target.value)}
+          className={styles.inputBuscar}
+        />
       </div>
-    
+      <hr className={styles.hr} />
+      <div className={styles.age}>
+        <p>Edad:</p>
+        <label className={styles.EdadMinMaxContainer}>
+          <span> Min </span>
+          <input
+            type="number"
+            placeholder='Min. 18'
+            value={minEdad}
+            onChange={(e) => setMinEdad(parseInt(e.target.value))}
+            min={18}
+            max={99}
+            className={styles.EdadMinMax}
+          />
+        </label>
+
+        <label className={styles.EdadMinMaxContainer}>
+          <span> Max </span>
+          <input
+            type="number"
+            placeholder='Max. 100'
+            value={maxEdad}
+            onChange={(e) => setMaxEdad(parseInt(e.target.value))}
+            min={19}
+            max={100}
+            className={styles.EdadMinMax}
+          />
+        </label>
+
+        <Button onClick={handleReset} size='sm' className={styles.btnReset}>Reset</Button>
+      </div>
+      <hr className={styles.hr} />
+      <div className={`${styles.cargHor} ${styles.checkBoxContainer}`}>
+        <p>Fulltime:</p>
+        <label>
+          <input
+            type="checkbox"
+            checked={fulltimeYes}
+            onChange={() => handleCheckboxChange('Si', 'Si')}
+            className={`form-check-input ${styles.checkBoxHover}`}
+          />
+          <span className={styles.checkBoxHover}> Si </span>
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={fulltimeNo}
+            onChange={() => handleCheckboxChange('No', 'Si')}
+            className={`form-check-input ${styles.checkBoxHover}`}
+          />
+          <span className={styles.checkBoxHover}> No </span>
+        </label>
+      </div>
+      <hr className={styles.hr} />
+      <div className={`${styles.mov} ${styles.checkBoxContainer}`}>
+        <p>Movilidad:</p>
+        <label>
+          <input
+            type="checkbox"
+            checked={movilidadYes}
+            onChange={() => handleCheckboxChange('Si', 'No')}
+            className={`form-check-input ${styles.checkBoxHover}`}
+          />
+          <span className={styles.checkBoxHover}> Si </span>
+        </label>
+
+        <label>
+          <input
+            type="checkbox"
+            checked={movilidadNo}
+            onChange={() => handleCheckboxChange('No', 'No')}
+            className={`form-check-input ${styles.checkBoxHover}`}
+          />
+          <span className={styles.checkBoxHover}> No </span>
+        </label>
+      </div>
+    </div>
+
   );
 };
